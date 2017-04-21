fa11dd812a223da7eda1c9068f251375c2b56f5b
Brad Gough <brad.m.gough@gmail.com>
2017-04-20 22:59:31 -1000

    Update build to create single file

--- build.bat
+++ build.bat
@@ -1,14 +1,20 @@
 @echo off
 
+del /q px.spec
 rmdir /s /q build
 rmdir /s /q __pycache__
+
 rmdir /s /q dist
 
-pyinstaller --clean --noupx -w -i px.ico px.py
-copy px.ini dist\px\.
-copy *.txt dist\px\.
-upx --best dist\px\*.pyd
-upx --best dist\px\py*.dll
+rem pyinstaller --clean --noupx -w -i px.ico px.py
+rem copy px.ini dist\px\.
+rem copy *.txt dist\px\.
+rem upx --best dist\px\*.pyd
+rem upx --best dist\px\py*.dll
+
+pyinstaller --clean --onefile -w -i px.ico px.py
+copy px.ini dist\.
+copy *.txt dist\.
 
 del /q px.spec
 rmdir /s /q build
